fc3b31738fd70bb17615520fdf4423040af0022c
blitzingeagle <mcmorrischen@gmail.com>
2018-05-30 09:50:15 +0000

    Hstack same number of channels

--- dec/display.py
+++ dec/display.py
@@ -3,9 +3,9 @@ import numpy as np
 
 x = np.random.random((50, 50, 3))
 y = np.random.random((50, 50, 3))
+z = np.hstack((x, y))
 
-cv2.imshow("LOL", x)
-cv2.imshow("LOL2", y)
+cv2.imshow("LOL", z)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
