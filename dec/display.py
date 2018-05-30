2889c83d73633e2d47d34a2b0bfcf7cf5e848b50
blitzingeagle <mcmorrischen@gmail.com>
2018-05-30 09:47:02 +0000

    Two displays

--- dec/display.py
+++ dec/display.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-Z = np.random.random((50, 50, 3))
-cv2.imshow("LOL", Z)
+x = np.random.random((50, 50, 3))
+y = np.random.random((50, 50, 3))
+
+cv2.imshow("LOL", x)
+cv2.imshow("LOL2", y)
 cv2.waitKey()
 cv2.destroyAllWindows()
