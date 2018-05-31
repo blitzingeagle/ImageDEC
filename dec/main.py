7e73e891edec09b4d600fc501f499dd6318781f3
blitzingeagle <mcmorrischen@gmail.com>
2018-05-31 02:03:59 +0000

    Depend main on imageset_utils

--- dec/main.py
+++ dec/main.py
@@ -3,63 +3,15 @@ from __future__ import print_function
 
 import dec
 import pretrain
+import imageset_utils as imgutils
 
-import numpy as np
 import cv2
+import numpy as np
 
 import os
 import os.path as path
-from glob import glob
-from operator import mul
 import shutil
 import time
-
-# === Imageset Loading ===
-def image_paths(directory):
-    return sorted(glob(path.join(directory, '*')))
-
-
-def load_images(filepaths, option=cv2.IMREAD_COLOR):
-    return [cv2.imread(filepath, option) for filepath in filepaths]
-
-
-def load_imageset(directory, option=cv2.IMREAD_COLOR):
-    return load_images(image_paths(directory), option)
-
-
-# === Imageset Stats ===
-def imageset_stats(imageset):
-    stats = {}
-    count = len(imageset)
-    shape = {}
-
-    height_sum, width_sum = 0, 0
-    for img in imageset:
-        height_sum += img.shape[0]
-        width_sum += img.shape[1]
-    shape["mean_height"] = height_sum / count
-    shape["mean_width"] = width_sum / count
-
-    stats["count"] = count
-    stats["shape"] = shape
-
-    return stats
-
-
-# === Resize Images ===
-def resize_images(images, shape=(128,128)):
-    return [cv2.resize(img, dsize=shape, interpolation=cv2.INTER_CUBIC) for img in images]
-
-
-def resize_to_mean(imageset):
-    imgset_stats = imageset_stats(imageset)
-    target_shape = (int(imgset_stats["shape"]["mean_width"]), int(imgset_stats["shape"]["mean_height"]))
-    return resize_images(imageset, shape=target_shape)
-
-
-# === Columnize ===
-def columnize(dataset):
-    return [elem.reshape(reduce(mul, elem.shape, 1)) for elem in dataset]
 
 
 def load_mnist(root, training):
@@ -265,30 +217,30 @@ if __name__ == "__main__":
     img_height = 50
     input_dim = img_width * img_height
 
-    data = columnize(resize_images(load_imageset(input_dir, cv2.IMREAD_GRAYSCALE), (img_width, img_height)))
+    data = imgutils.columnize(imgutils.resize_images(imgutils.load_imageset(input_dir, cv2.IMREAD_GRAYSCALE), (img_width, img_height)))
 
-    make_image_data(data)
-
-    pretrain.main(db, {
-        'n_layer': [4],
-        'dim': [input_dim, 500, 500, 2000, 10],
-        'drop': [0.0],
-        'rate': [0.1],
-        'step': [20000],
-        'iter': [100000],
-        'decay': [0.0000]
-    })
-
-    pretrain.pretrain_main(db, {
-        'dim': [input_dim, 500, 500, 2000, 10],
-        'pt_iter': [50000],
-        'drop': [0.2],
-        'rate': [0.1],
-        'step': [20000],
-        'iter': [100000],
-        'decay': [0.0000]
-    })
-
-    os.system("caffe train --solver=ft_solver.prototxt --weights=stack_init_final.caffemodel")
-
-    DisKmeans()
+    # make_image_data(data)
+    #
+    # pretrain.main(db, {
+    #     'n_layer': [4],
+    #     'dim': [input_dim, 500, 500, 2000, 10],
+    #     'drop': [0.0],
+    #     'rate': [0.1],
+    #     'step': [20000],
+    #     'iter': [100000],
+    #     'decay': [0.0000]
+    # })
+    #
+    # pretrain.pretrain_main(db, {
+    #     'dim': [input_dim, 500, 500, 2000, 10],
+    #     'pt_iter': [50000],
+    #     'drop': [0.2],
+    #     'rate': [0.1],
+    #     'step': [20000],
+    #     'iter': [100000],
+    #     'decay': [0.0000]
+    # })
+    #
+    # os.system("caffe train --solver=ft_solver.prototxt --weights=stack_init_final.caffemodel")
+    #
+    # DisKmeans()
